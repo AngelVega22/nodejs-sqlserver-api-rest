4cca72e37379299c9395e004a2b4df92ab9c4034
Angel Vega <avegasam@gmail.com>
2022-10-26 17:21:34 -0500

    update

--- dist/controllers/indexController.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-exports.index = function (req, res, next) {
-  res.send({
-    Express: 'Working'
-  });
-};
--- dist/routes/reports.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var _reportsController = require("../controllers/reportsController");
-var express = require('express');
-var router = express.Router();
-/* GET home page. */
-
-router.get('/test', _reportsController.apiTesting);
-module.exports = router;
